f040b3fd34f0ea8aa7c52ad6f32e695a11d32138
Ryan <ryeean16@gmail.com>
2023-08-29 10:06:59 -0700

    troubleshoot and warning light page functional

--- src/pages/TroubleshootPage/TroubleshootPage.jsx
+++ src/pages/TroubleshootPage/TroubleshootPage.jsx
@@ -1,51 +1,73 @@
-import React, { useState } from 'react';
-import axios from 'axios'; 
-import SecondHeaderComponent from '../../components/SecondHeaderComponent/SecondHeaderComponent';
-import './TroubleshootPage.scss';
+import React, { useState, useEffect } from "react";
+import axios from "axios";
+import SecondHeaderComponent from "../../components/SecondHeaderComponent/SecondHeaderComponent";
+import "./TroubleshootPage.scss";
 
-const PORT = process.env.REACT_APP_API_SERVER
+const PORT = process.env.REACT_APP_API_SERVER;
 
 function TroubleshootPage() {
-    const [userQuestion, setUserQuestion] = useState('');
-    const [response, setResponse] = useState('');
+  const [selectedIssue, setSelectedIssue] = useState("");
+  const [issues, setIssues] = useState([]);
+  const [response, setResponse] = useState([]);
 
-    const handleUserQuestionChange = (event) => {
-        setUserQuestion(event.target.value);
-    };
+  useEffect(() => {
+    axios.get(`${PORT}/troubleshootPage/issues`)
+      .then((response) => {
+        setIssues(response.data);
+      })
+      .catch((error) => {
+        console.error("Error fetching issues:", error);
+      });
+  }, []);
 
-    const handleSubmit = () => {
-        axios.post(`${PORT}`, { question: userQuestion })
-            .then(response => {
-                setResponse(response.data.response);
-            })
-            .catch(error => {
-                console.error('Error fetching response:', error);
-            });
-    };
+  const handleIssueChange = (event) => {
+    setSelectedIssue(event.target.value);
+  };
 
-    return (
-        <section className="troubleshoot">
-            <div className="troubleshoot__header">
-            <SecondHeaderComponent />
+  const handleSubmit = () => {
+    axios
+      .post(`${PORT}/troubleshootPage`, { question: selectedIssue })
+      .then((response) => {
+        setResponse(response.data);
+      })
+      .catch((error) => {
+        console.error("Error fetching response:", error);
+      });
+  };
+
+  return (
+    <section className="troubleshoot">
+      <div className="troubleshoot__header">
+        <SecondHeaderComponent />
+      </div>
+      <div className="troubleshoot__question">
+        <h2>Let's try to find a solution!</h2>
+<div className="troubleshoot__selectandbutton">
+        <select value={selectedIssue} onChange={handleIssueChange}>
+          <option value="">Select an issue...</option>
+          {issues.map((issue) => (
+            <option key={issue.id}>
+              {issue.problem}
+            </option>
+          ))}
+        </select>
+        <button onClick={handleSubmit}>Submit</button>
+      </div>
+      </div>
+      {response.length > 0 && (
+        <div className="troubleshoot__response">
+          {response.map((solution, index) => (
+            <div className="troubleshoot__solutions" key={index}>
+              <h1 className="troubleshoot__solutions--title">Solution {index + 1}</h1>
+              <p className="troubleshoot__solutions--solution">Solution: {solution.solution}</p>
+              <p className="troubleeshoot__solutions--instructions">Instructions: {solution.instructions}</p>
+              <p className="troubleshoot__solutions--tools">Tools Required: {solution.tools_required}</p>
             </div>
-            <div className="questionSection">
-                <h2>Ask a Question</h2>
-                <textarea
-                    rows="4"
-                    placeholder="Type your question here..."
-                    value={userQuestion}
-                    onChange={handleUserQuestionChange}
-                />
-                <button onClick={handleSubmit}>Submit</button>
-            </div>
-            {response && (
-                <div className="responseSection">
-                    <h2>Response</h2>
-                    <p>{response}</p>
-                </div>
-            )}
-        </section>
-    );
+          ))}
+        </div>
+      )}
+    </section>
+  );
 }
 
 export default TroubleshootPage;
--- src/pages/WarningLightsPage/WarningLightsPage.jsx
+++ src/pages/WarningLightsPage/WarningLightsPage.jsx
@@ -1,35 +1,86 @@
-import React from 'react';
+import React, { useState } from 'react';
 import SecondHeaderComponent from "../../components/SecondHeaderComponent/SecondHeaderComponent";
 import "./WarningLightsPage.scss";
-import oilChangeImage from '../../assets/garage.jpeg';
-import tiresImage from '../../assets/maint.jpg';
-import brakePadsImage from '../../assets/openhood.jpg';
+import absIcon from '../../assets/ABS-light.png';
+import airbagIcon from '../../assets/airbag-indicator.png';
+import batteryIcon from '../../assets/battery-alert.png';
+import brakeIcon from '../../assets/brake-warning.png';
+import checkEngingIcon from '../../assets/check-engine-light.png';
+import engingTemperatureIcon from '../../assets/engine-temperature.png';
+import foglampIcon from '../../assets/fog-lamp.png';
+import laneDepartureIcon from '../../assets/lane-departure.png';
+import lowFuelIcon from '../../assets/low-fuel.png';
+import oilPressureIcon from '../../assets/oil-pressure-light.png';
+import seatBeltIcon from '../../assets/seat-belt.png';
+import securityIcon from '../../assets/security-indicator.png';
+import shiftLockIcon from '../../assets/shift-lock.png';
+import tirePressureIcon from '../../assets/tire-pressure-light.png';
+import tractionControlIcon from '../../assets/traction-control.png';
+import tractionControlMalfunctionIcon from '../../assets/traction-control-malfunction.png';
+import transmissionTempIcon from '../../assets/transmission-temperature.png';
+import washerFluidIcon from '../../assets/washer-fluid.png';
+
 
 function WarningLightsPage() {
+  const [selectedWarning, setSelectedWarning] = useState(null);
+
+  const warningLightsData = [
+    { id: 1, title: 'Oil Pressure', description: 'Oil pressure is low.', icon: oilPressureIcon },
+    { id: 2, title: 'Check Engine Light', description: 'Use a DTC scanner.', icon: checkEngingIcon },
+    { id: 3, title: 'ABS System', description: 'ABS system malfunction.', icon: absIcon },
+    { id: 4, title: 'Airbag Indicator', description: 'Airbag system fault.', icon: airbagIcon },
+    { id: 5, title: 'Battery Alert', description: 'Battery voltage is low.', icon: batteryIcon },
+    { id: 6, title: 'Brake Alert', description: 'Brake system warning.', icon: brakeIcon },
+    { id: 7, title: 'Engine Temperature', description: 'Engine temperature is high.', icon: engingTemperatureIcon },
+    { id: 8, title: 'Fog Lamp Indicator', description: 'Fog lamps are on.', icon: foglampIcon },
+    { id: 9, title: 'Lane Departure Warning', description: 'Vehicle is drifting from lane.', icon: laneDepartureIcon },
+    { id: 10, title: 'Low Fuel', description: 'Fuel level is low.', icon: lowFuelIcon },
+    { id: 11, title: 'Seat Belt Reminder', description: 'Seat belt not fastened.', icon: seatBeltIcon },
+    { id: 12, title: 'Security Indicator', description: 'Security system active.', icon: securityIcon },
+    { id: 13, title: 'Shift Lock Indicator', description: 'Shift lock engaged.', icon: shiftLockIcon },
+    { id: 14, title: 'Tire Pressure', description: 'Tire pressure is low.', icon: tirePressureIcon },
+    { id: 15, title: 'Traction Control', description: 'Traction control system active.', icon: tractionControlIcon },
+    { id: 16, title: 'Traction Control Malfunction', description: 'Traction control system malfunction.', icon: tractionControlMalfunctionIcon },
+    { id: 17, title: 'Transmission Temperature', description: 'Transmission temperature is high.', icon: transmissionTempIcon },
+    { id: 18, title: 'Washer Fluid Low', description: 'Washer fluid level is low.', icon: washerFluidIcon }
+    // Add more warning lights data
+  ];
+  
+
+  const handleWarningClick = (warningId) => {
+    setSelectedWarning(warningId);
+  };
+
+  const closeWarningModal = () => {
+    setSelectedWarning(null);
+  };
+
   return (
     <section className="warningLights">
       <div className="WarningLights__header">
         <SecondHeaderComponent />
       </div>
+      <h1>Warning Lights</h1>
       <div className="warningLights__content">
-        <div className="warningLights__title">
-          <h1>Warning Lights</h1>
-        </div>
-        <ul className="warningLights__information">
-          <li>
-            <img src={oilChangeImage} alt="Oil Change" />
-            <p>Check Engine</p>
-          </li>
-          <li>
-            <img src={tiresImage} alt="Tires" />
-            <p>Brake Lights</p>
-          </li>
-          <li>
-            <img src={brakePadsImage} alt="Brake Pads" />
-            <p>Oil Pressure</p>
-          </li>
-        </ul>
+          {warningLightsData.map(warning => (
+            <div
+              key={warning.id}
+              className="warningLights__icon"
+              onClick={() => handleWarningClick(warning.id)}
+            >
+              <img src={warning.icon} alt={warning.title} />
+            </div>
+          ))}
       </div>
+      {selectedWarning !== null && (
+        <div className="warningLights-modal">
+          <div className="warningLights__modal--content">
+            <h2>{warningLightsData[selectedWarning - 1].title}</h2>
+            <p>{warningLightsData[selectedWarning - 1].description}</p>
+            <button onClick={closeWarningModal}>Close</button>
+          </div>
+        </div>
+      )}
     </section>
   );
 }

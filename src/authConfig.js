349693633afbc488fd5b0645d2125a01540a637c
Ryan <ryeean16@gmail.com>
2023-12-12 03:10:52 -0800

    update

--- src/authConfig.js
+++ src/authConfig.js
@@ -2,7 +2,7 @@
 
 const AuthConfig = {
     domain: process.env.REACT_APP_AUTH0_DOMAIN,
-    clientId: process.env.REACTAPP_AUTH0_CLIENT_ID,
+    clientId: process.env.REACT_APP_AUTH0_CLIENT_ID,
     redirectUri: "https://cruisin.netlify.app",
     googleClientId: process.env.REACT_APP_GOOGLE_CLIENT_ID,
     githubClientId: process.env.REACT_APP_GITHUB_CLIENT_ID,
